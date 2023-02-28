1a078e980d6e104d08a6ea066a67066dbcd77de1
Krishna Lagad <krishnalagad2@gmail.com>
2023-02-28 16:33:17 +0530

    Update Week1Day5_StringOperations_Adjecant.java

--- Week1Day5_StringOperations_Adjecant.java
+++ Week1Day5_StringOperations_Adjecant.java
@@ -4,7 +4,7 @@ public class Week1Day5_StringOperations_Adjecant {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the string");
-        String str = sc.next().trim();
+        String str = sc.nextLine().trim();
         System.out.println("The processed string is " + separateAdjacentCharacters(str));
     }
 
