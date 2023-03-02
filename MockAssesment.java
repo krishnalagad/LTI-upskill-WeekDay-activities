5c3a7c5a957ab9c6bf2e205127d9e4a9a0c6fb02
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-03 00:03:37 +0530

    - FINAL DUMP

--- MockAssesment.java
+++ MockAssesment.java
@@ -3,16 +3,12 @@ import java.util.*;
 public class MockAssesment {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter size of array:");
         int size = sc.nextInt();
         List<Float> list = new ArrayList<Float>();
-        System.out.println("Enter elements:");
         for (int i = 0; i < size; i++)
             list.add(sc.nextFloat());
 
-        System.out.println("Enter position to enter element:");
         int pos = sc.nextInt();
-        System.out.println("Enter data to insert:");
         float data = sc.nextFloat();
 
         list.add(pos - 1, data);
