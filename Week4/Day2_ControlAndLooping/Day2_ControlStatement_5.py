a04c4b9906e00d5423768c35ff9ce781106fcede
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-22 19:41:40 +0530

    Update Day2_ControlStatement_5.py

--- Week4/Day2_ControlAndLooping/Day2_ControlStatement_5.py
+++ Week4/Day2_ControlAndLooping/Day2_ControlStatement_5.py
@@ -1,18 +1,10 @@
-if __name__ == "__main__":
-    width = int(input())
-    length = int(input())
-    width_feet = width / 12
-    length_feet = length / 12
-    total_area = width_feet * length_feet
-    tiles_needed = int(total_area // 0.44)
-    partial_area = total_area % 0.44
-    if partial_area > 0:
-        if partial_area <= 0.22:
-            partial_tiles_needed = 1
-        else:
-            partial_tiles_needed = 2
-    else:
-        partial_tiles_needed = 0
-    print(tiles_needed)
-    print(partial_tiles_needed)
+width = int(input())
+length = int(input())
 
+total_area = width * length
+tile_area = 8 * 8
+
+whole_tiles = total_area // tile_area
+part_tiles = 1 if total_area % tile_area > 0 else 0
+
+print(whole_tiles, part_tiles)
