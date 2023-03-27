08b3abd15b475c92bff17abda2c1805145bc7ee8
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-27 20:12:21 +0530

    week4 problem4: solution dump

---
+++ Week4/Day4_Inheritance/Problem3_SuperKeyward/Exhibition.java
@@ -0,0 +1,22 @@
+package Problem3_SuperKeyward;
+
+class Exhibition extends Event {
+    private int noOfStalls;
+
+    public Exhibition() {
+        super();
+    }
+
+    public Exhibition(String name, String detail, String type, String organiserName, int noOfStalls) {
+        super(name, detail, type, organiserName);
+        this.noOfStalls = noOfStalls;
+    }
+
+    public int getNoOfStalls() {
+        return noOfStalls;
+    }
+
+    public void setNoOfStalls(int noOfStalls) {
+        this.noOfStalls = noOfStalls;
+    }
+}
