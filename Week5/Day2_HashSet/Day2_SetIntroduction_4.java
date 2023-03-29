0734b5709794aef1867712925e265d0c59f8703c
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-29 22:26:59 +0530

    - requires fix.

--- Week5/Day2_HashSet/Day2_SetIntroduction_4.java
+++ Week5/Day2_HashSet/Day2_SetIntroduction_4.java
@@ -1,21 +1,83 @@
 import java.util.*;
 
+class User implements Comparable<User>{
+    private String username;
+    private String bankname;
+    public User() {
+    }
+    public User(String username, String bankname) {
+        this.username = username;
+        this.bankname = bankname;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getBankname() {
+        return bankname;
+    }
+    public void setBankname(String bankname) {
+        this.bankname = bankname;
+    }
+    @Override
+    public int compareTo(User o) {
+        return this.username.compareTo(o.username);
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        User other = (User) obj;
+        return username.equals(other.username);
+    }    
+}
+
 public class Day2_SetIntroduction_4 {
     public static void main(String[] args) {
-        Set<String> set = new HashSet<String>();
-        Scanner sc = new Scanner(System.in);
-        boolean flag = true;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of users:");
+        int n = scanner.nextInt();
+        scanner.nextLine();
 
-        while (flag) {
-            System.out.println("Enter the username");
-            String username = sc.next().trim();
-            set.add(username);
-            System.out.println("Do you want to continue?(Y/N)");
-            String str = sc.next().trim();
-            if (!str.equalsIgnoreCase("y")) {
-                flag = false;
+        Set<User> users = new HashSet<>();
+        List<User> userList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter details of user" + (i + 1));
+            System.out.println("Username:");
+            String username = scanner.nextLine();
+            System.out.println("Bank name:");
+            String bankname = scanner.nextLine();
+            User user = new User(username, bankname);
+            users.add(user);
+            userList.add(new User(username, null));
+        }
+
+        System.out.println("Enter username(Expire in one month) separated by comma");
+        String[] expiredUsernames = scanner.nextLine().split(",");
+
+        List<User> expiredUsers = new ArrayList<>();
+        for (String username : expiredUsernames) {
+            User user = new User(username.trim(), null);
+            if (userList.contains(user)) {
+                expiredUsers.add(user);
             }
         }
-        System.out.println("The unique number of usernames is " + set.size());
+
+        users.retainAll(userList);
+
+        System.out.println("Users going to expire within a month");
+        int count = 1;
+        for (User user : expiredUsers) {
+            int index = userList.indexOf(user);
+            User userInList = userList.get(index);
+            System.out.println("User " + count++);
+            System.out.println("User Name = " + userInList.getUsername());
+            boolean flag = users.contains(userInList);
+            System.out.println("Bank Name = " + (flag ? userInList.getBankname() : "Not Found"));
+        }
     }
 }
