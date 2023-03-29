2c5e89f229f2d4547f2f355d2ae58b75dba2d723
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-29 22:02:53 +0530

    - problem 3 solution.

--- Week5/Day2_HashSet/Day2_SetIntroduction_3.java
+++ Week5/Day2_HashSet/Day2_SetIntroduction_3.java
@@ -1,21 +1,49 @@
 import java.util.*;
 
 public class Day2_SetIntroduction_3 {
+
+    static boolean isPresent(String[] emails, Set<String> set) {
+        int flag = 0;
+        for (String email : emails) {
+            if (set.contains(email))
+                flag = 1;
+            else
+                flag = 0;
+        }
+
+        return (flag == 1) ? true : false;
+    }
+
     public static void main(String[] args) {
         Set<String> set = new HashSet<String>();
         Scanner sc = new Scanner(System.in);
-        boolean flag = true;
+        Set<String> emails = new HashSet<String>();
 
-        while (flag) {
-            System.out.println("Enter the username");
-            String username = sc.next().trim();
-            set.add(username);
-            System.out.println("Do you want to continue?(Y/N)");
-            String str = sc.next().trim();
-            if (!str.equalsIgnoreCase("y")) {
-                flag = false;
+        while (true) {
+            System.out.println("Enter Email address");
+            String email = sc.nextLine();
+
+            if (email.isEmpty()) {
+                // continue;
+            }
+
+            if (emails.contains(email)) {
+            } else
+                emails.add(email);
+
+            System.out.println("Do you want to Continue?(yes/no)");
+            String choice = sc.nextLine().trim().toLowerCase();
+            if (choice.equalsIgnoreCase("no")) {
+                break;
             }
         }
-        System.out.println("The unique number of usernames is " + set.size());
+        System.out.println("Enter the email addresses to be searched separated by comma");
+        String searchStr = sc.next().trim();
+        String[] emailArr = searchStr.split(",");
+        if (isPresent(emailArr, emails)) {
+            System.out.println("Email addresses are present");
+        } else {
+            System.out.println("Email addresses are not present");
+        }
     }
 }
