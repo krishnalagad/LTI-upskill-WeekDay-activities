c98742d2ee5722daf436ec9e6a1912288e903414
Krishna Lagad <krishnalagad2@gmail.com>
2023-03-31 15:57:03 +0530

    Solution 5 dump

---
+++ Week4/Day5_ObjectRelationshipPolymorphism/Problem5_AreaOfShape/Circle.java
@@ -0,0 +1,24 @@
+package Problem5_AreaOfShape;
+
+public class Circle extends Shape {
+    private Double radius;
+
+    public Circle() {
+    }
+
+    public Circle(Double radius) {
+        this.radius = radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return this.radius;
+    }
+
+    public void computeArea() {
+        super.setArea((3.142857) * (radius * radius));
+    }
+}
---
+++ Week4/Day5_ObjectRelationshipPolymorphism/Problem5_AreaOfShape/Shape.java
@@ -0,0 +1,24 @@
+package Problem5_AreaOfShape;
+
+public class Shape {
+    protected Double area;
+
+    public Shape() {
+    }
+
+    public Shape(Double area) {
+        this.area = area;
+    }
+
+    public void setArea(Double area) {
+        this.area = area;
+    }
+
+    public Double getArea() {
+        return this.area;
+    }
+
+    public void computeArea() {
+        this.setArea(0.0);
+    }
+}
