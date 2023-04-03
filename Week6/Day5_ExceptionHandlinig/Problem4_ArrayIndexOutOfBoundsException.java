6e5e1b6895cedc56bbfcb2394378edc0698dc75d
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-03 15:03:05 +0530

    - requires fix

--- Week6/Day5_ExceptionHandlinig/Problem4_ArrayIndexOutOfBoundsException.java
+++ Week6/Day5_ExceptionHandlinig/Problem4_ArrayIndexOutOfBoundsException.java
@@ -2,27 +2,37 @@ import java.util.*;
 
 public class Problem4_ArrayIndexOutOfBoundsException {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of seats to be booked:");
-        int no = sc.nextInt();
-        int[] seats = new int[no];
+        int[] seatArray = new int[100];
+        Scanner scanner = new Scanner(System.in);
 
-        try {
-            for (int i = 0; i < no; i++) {
+        // Get the number of seats to be booked from the user
+        System.out.println("Enter the number of seats to be booked:");
+        int numOfSeats = scanner.nextInt();
+
+        // Get the seat numbers from the user and handle any exception that occurs
+        for (int i = 0; i < numOfSeats; i++) {
+            try {
                 System.out.println("Enter the seat number " + (i + 1));
-                int seat = sc.nextInt();
-                if (seat <= 100) {
-                    seats[i] = seat;
-                } else {
+                int seatNumber = scanner.nextInt();
+
+                // Check if the seat number is within the valid range
+                if (seatNumber < 1 || seatNumber > 100) {
                     throw new ArrayIndexOutOfBoundsException("100");
                 }
+
+                // Book the seat
+                seatArray[seatNumber - 1] = seatNumber;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(e);
             }
-            System.out.println("The seats booked are:");
-            for (int seatNo : seats) {
-                System.out.println(seatNo);
+        }
+
+        // Display all the tickets booked
+        System.out.println("The seats booked are:");
+        for (int seatNumber : seatArray) {
+            if (seatNumber != 0) {
+                System.out.println(seatNumber);
             }
-        } catch (Exception e) {
-            System.out.println(e.toString());
         }
     }
 }
