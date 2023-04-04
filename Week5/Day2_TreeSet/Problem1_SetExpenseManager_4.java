166c8c2822b076171cbb348faabe1798a062a3bf
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-04 19:02:10 +0530

    - dump solution 4.

--- Week5/Day2_TreeSet/Problem1_SetExpenseManager_4.java
+++ Week5/Day2_TreeSet/Problem1_SetExpenseManager_4.java
@@ -3,11 +3,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.TreeSet;
 
-public class Main {
+public class Problem1_SetExpenseManager_4 {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
