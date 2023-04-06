451decaf19e485bad1461897896833bc72ea88ff
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-06 16:47:33 +0530

    - require fix.

---
+++ Week5/Day5_ComparatorComparable/Problem1_/AccountBalanceComparator.java
@@ -0,0 +1,15 @@
+
+import java.util.Comparator;
+
+public class AccountBalanceComparator implements Comparator<Account> {
+
+    public AccountBalanceComparator() {
+        super();
+    }
+
+    @Override
+    public int compare(Account o1, Account o2) {
+        return o1.getAccountBalance().compareTo(o2.getAccountBalance());
+    }
+
+}
