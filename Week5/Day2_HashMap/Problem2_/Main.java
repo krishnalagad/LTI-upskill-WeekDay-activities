e33f274d54486edc250c35dc1eccc74976676ff0
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-06 22:55:32 +0530

    - dump solution 2.

---
+++ Week5/Day2_HashMap/Problem2_/Main.java
@@ -0,0 +1,24 @@
+package Problem2_;
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < n; i++) {
+            map.put(sc.nextInt(), sc.nextInt());
+        }
+
+        UserMainCode.findPass(map);
+    }
+
+}
