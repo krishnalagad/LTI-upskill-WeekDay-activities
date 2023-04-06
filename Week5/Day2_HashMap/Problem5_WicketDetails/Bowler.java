1a6989ffe53dcf0b210394ae117935431656575f
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-06 23:02:44 +0530

    - dump solution 5.

---
+++ Week5/Day2_HashMap/Problem5_WicketDetails/Bowler.java
@@ -0,0 +1,19 @@
+package Problem5_WicketDetails;
+
+public class Bowler {
+    private String name;
+
+    public Bowler(String name) {
+        super();
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ Week5/Day2_HashMap/Problem5_WicketDetails/Wicket.java
@@ -0,0 +1,29 @@
+package Problem5_WicketDetails;
+
+public class Wicket {
+    private String playerName;
+    private Bowler Bowler;
+
+    public Wicket(String playerName, Bowler bowler) {
+        super();
+        this.playerName = playerName;
+        Bowler = bowler;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public Bowler getBowler() {
+        return Bowler;
+    }
+
+    public void setBowler(Bowler bowler) {
+        Bowler = bowler;
+    }
+
+}
