14dbff603a9ad2ec38fdf5165d17e13422cfc40b
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-21 15:55:51 +0530

    - requires fix.

---
+++ Week8/Day5_DOMEventListeners/Problem4/scrips.js
@@ -0,0 +1,43 @@
+function hoverRow1() {
+  document.getElementById("accNumber").innerHTML =
+    "Acc Number:8456844568845" + "</br>";
+  document.getElementById("balance").innerHTML = "Balance:1223.46";
+  document.getElementById("details").style.display = "block";
+}
+
+function removeHover1() {
+  document.getElementById("details").style.display = "none";
+}
+
+function hoverRow2() {
+  document.getElementById("accNumber").innerHTML =
+    "Acc Number:8453333568845" + "</br>";
+  document.getElementById("balance").innerHTML = "Balance:223.46";
+  document.getElementById("details").style.display = "block";
+}
+
+function removeHover2() {
+  document.getElementById("details").style.display = "none";
+}
+
+function hoverRow3() {
+  document.getElementById("accNumber").innerHTML =
+    "Acc Number:8456882214345" + "</br>";
+  document.getElementById("balance").innerHTML = "Balance:123.46";
+  document.getElementById("details").style.display = "block";
+}
+
+function removeHover3() {
+  document.getElementById("details").style.display = "none";
+}
+
+function hoverRow4() {
+  document.getElementById("accNumber").innerHTML =
+    "Acc Number:4554589568845" + "</br>";
+  document.getElementById("balance").innerHTML = "Balance:13.46";
+  document.getElementById("details").style.display = "block";
+}
+
+function removeHover4() {
+  document.getElementById("details").style.display = "none";
+}
