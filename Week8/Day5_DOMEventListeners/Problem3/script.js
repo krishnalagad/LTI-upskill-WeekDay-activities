79c171a9ad4925ff2070288ff2b1b73b0b21eb17
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-22 17:29:49 +0530

    - dump solution 3.

--- Week8/Day5_DOMEventListeners/Problem3/script.js
+++ Week8/Day5_DOMEventListeners/Problem3/script.js
@@ -1,9 +1,24 @@
-function capitalizeFirstLetter(input) {
-  var value = input.value;
-  input.value = value.charAt(0).toUpperCase() + value.slice(1);
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const but = document.querySelector("#register").classList;
+function myfun() {
+  firstname.value =
+    firstname.value.charAt(0).toUpperCase() +
+    firstname.value.slice(1).toLowerCase();
+}
+firstname.onblur = function () {
+  myfun();
+};
+lastname.onblur = function () {
+  myfuncL();
+};
+
+function myfuncL() {
+  lastname.value =
+    lastname.value.charAt(0).toUpperCase() +
+    lastname.value.slice(1).toLowerCase();
 }
 
-function changeBackgroundColor() {
-  var button = document.getElementsByTagName("button")[0];
-  button.classList.add("focused");
+function nun() {
+  but.add("mycol");
 }
