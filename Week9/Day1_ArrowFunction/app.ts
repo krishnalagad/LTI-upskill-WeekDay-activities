018f5afb68419ab4e2b3788db816a53324678f9d
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-26 12:55:01 +0530

    - dump updated solution.

--- Week9/Day1_ArrowFunction/app.ts
+++ Week9/Day1_ArrowFunction/app.ts
@@ -1,20 +1,21 @@
-import * as fs from "fs";
+declare var require: any;
+let fs = require("fs");
 
 fs.readFile("input.txt", "utf8", (err: any, data: string) => {
-    if (err) throw err;
+  if (err) throw err;
 
-    const year: number = parseInt(data);
-    console.log(isLeapYear(year));
+  const year: number = parseInt(data);
+  console.log(isLeapYear(year));
 });
 
 const isLeapYear = (year: number): boolean => {
-    if (year % 4 !== 0) {
-      return false;
-    } else if (year % 100 !== 0) {
-      return true;
-    } else if (year % 400 !== 0) {
-      return false;
-    } else {
-      return true;
-    }
-}
+  if (year % 4 !== 0) {
+    return false;
+  } else if (year % 100 !== 0) {
+    return true;
+  } else if (year % 400 !== 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
