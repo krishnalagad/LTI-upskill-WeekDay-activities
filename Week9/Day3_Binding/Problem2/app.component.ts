0b72affa20a722e80e86a0a6c7a6b934d77b5567
Krishna Lagad <krishnalagad2@gmail.com>
2023-04-28 21:17:19 +0530

    - dump solution 2.

---
+++ Week9/Day3_Binding/Problem2/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  //Fill your code here
+  photosHeading = "Photos";
+  photos = [
+    { id: "picture-1", url: "../assets/images/product1.jpg" },
+    { id: "picture-2", url: "../assets/images/product2.jpg" },
+    { id: "picture-3", url: "../assets/images/product3.jpg" },
+    { id: "picture-4", url: "../assets/images/product4.jpg" },
+    { id: "picture-5", url: "../assets/images/product5.jpg" },
+    { id: "picture-6", url: "../assets/images/product6.jpg" },
+    { id: "picture-7", url: "../assets/images/product7.jpg" },
+    { id: "picture-8", url: "../assets/images/product8.jpg" },
+  ];
+}
