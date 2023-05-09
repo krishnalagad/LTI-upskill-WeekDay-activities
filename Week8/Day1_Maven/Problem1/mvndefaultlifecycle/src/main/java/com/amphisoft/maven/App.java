51d90b85f53685b49478798c6f77ae2d4dcd8700
Krishna Lagad <krishnalagad2@gmail.com>
2023-05-09 13:11:38 +0530

    Delete Week8/Day1_Maven directory

--- Week8/Day1_Maven/Problem1/mvndefaultlifecycle/src/main/java/com/amphisoft/maven/App.java
+++
@@ -1,9 +0,0 @@
-package com.amphisoft.maven;
-public class App {
-    public static void main(String[] args) {
-        System.out.println(obtainWelcomeString());
-    }
-    public static String obtainWelcomeString() {
-		return "Hello Karthick. Welcome to maven!!!";
-	}
-}
